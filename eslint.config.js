71089d53b03fe10aef182d85ef657bd3c99a9758
Joao Gomes <jg0112360@gmail.com>
2025-12-24 01:01:06 -0300

    feat: add prisma client

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
-import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
-import tseslint from "typescript-eslint";
+//import js from "@eslint/js";
+//import tsParser from "@typescript-eslint/parser";
+//import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
+//import tseslint from "typescript-eslint";
 
-export default [
-  ...tseslint.configs.strict,
-  {
-    files: ["**/*.{js,mjs,ts}"],
-  },
-  {
-    ignores: ["**/node_modules", "dist/"],
-  },
-  js.configs.recommended,
-  {
-    languageOptions: {
-      parser: tsParser,
-    },
-    plugins: {
-      "@typescript-eslint": tsEslintPlugin,
-    },
-    rules: {
-      "no-undef": "warn",
-      "no-unused-vars": "warn",
-      semi: ["error", "never"],
-    },
-  },
-];
+//export default [
+  //...tseslint.configs.strict,
+  //{
+    //files: ["**/*.{js,mjs,ts}"],
+  //},
+  //{
+    //ignores: ["**/node_modules", "dist/"],
+  //},
+  //js.configs.recommended,
+  //{
+    //languageOptions: {
+      //parser: tsParser,
+    //},
+    //plugins: {
+      //"@typescript-eslint": tsEslintPlugin,
+    //},
+    //rules: {
+      //"no-undef": "warn",
+      //"no-unused-vars": "warn",
+      //semi: ["error", "never"],
+    //},
+  //},
+//];
