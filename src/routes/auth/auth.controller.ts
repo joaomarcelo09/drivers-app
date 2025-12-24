439a11483b87ea65121f3db3e83a48ca4dd9e63b
Joao Gomes <jg0112360@gmail.com>
2025-12-24 12:25:20 -0300

    feat: jwt logic

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import { createUser } from '../../services/user.service';
 import { validateData } from '../../middleware/validationMiddleware';
 import { userLoginSchema } from '../../schemas/userSchema';
+import authMiddleware from '../../middleware/securityMiddleware';
+import generateToken from '../../utils/generateToken';
 
 const router = Router();
 
@@ -14,12 +15,12 @@ const router = Router();
  */
 router.post('/login', validateData(userLoginSchema), async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const response = await createUser();
+    const response = generateToken(1);
     res.json({ response });
   } catch (error) {
     next(error);
   }
 });
 
-export default Router().use('/auth', router);
+export default Router().use('/auth', authMiddleware.optional,router);
 
