d570fa951c7f2e2874f0c40e5d5bf371d13303ac
juliaam <juliademoraess@gmail.com>
2026-01-12 18:15:12 -0300

    chore: configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes/routes';
-import HttpException from './models/http-exception.model';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import routes from "./routes/routes";
+import HttpException from "./models/http-exception.model";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,42 +10,42 @@ const app = express();
  * App Configuration
  */
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(cookieParser());
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(routes);
 
 // Serves images
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + "/assets"));
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.json({ status: 'API is running on /api' });
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.json({ status: "API is running on /api" });
 });
 
 /* eslint-disable */
-app.use(
-  (
-    err: Error | HttpException,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-  ) => {
+app.use((err: Error | HttpException, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  // @ts-ignore
+  if (err && err.name === "UnauthorizedError") {
+    return res.status(401).json({
+      status: "error",
+      message: "missing authorization credentials",
+    });
     // @ts-ignore
-    if (err && err.name === 'UnauthorizedError') {
-      return res.status(401).json({
-        status: 'error',
-        message: 'missing authorization credentials',
-      });
-      // @ts-ignore
-    } else if (err && err.errorCode) {
-      // @ts-ignore
-      res.status(err.errorCode).json(err.message);
-    } else if (err) {
-      res.status(500).json(err.message);
-    }
-  },
-);
+  } else if (err && err.errorCode) {
+    // @ts-ignore
+    res.status(err.errorCode).json(err.message);
+  } else if (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 /**
  * Server activation
